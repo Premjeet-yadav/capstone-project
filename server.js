eb803dc177175d42476d6761d610a33cb5896fff
Premjeet-yadav <ypremjeet360@gmail.com>
2023-06-29 11:06:45 +0530

    4thd

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const PORT = 5000
-const path= require('path')
 const jsonServer= require('json-server')
 const server = jsonServer.create()
-const router= jsonServer.router(path.join(__dirname,'db.json'))
+const router= jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 server.use(middlewares)
 server.use(jsonServer.bodyParser)
-server.use('/playo',router)
+server.use('',router)
 server.listen(PORT, ()=> console.log(`JSON Server is running on port ${PORT}`))
